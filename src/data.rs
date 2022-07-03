36c73f0f8c5aabd28d470cc0b81272678e31fd55
Qin Wan <rogerwq@gmail.com>
2022-07-03 13:09:25 +0900

    v0.1.0

--- src/data.rs
+++
@@ -1,5 +0,0 @@
-use chiral_db;
-
-pub fn load_fp_chembl(fpk: &chiral_db::fingerprint::Kind) -> chiral_db::fingerprint::FingerprintDocument {
-    chiral_db::fingerprint::FingerprintDocument::new_from_chembl(fpk)
-}
